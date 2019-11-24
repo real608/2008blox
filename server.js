17042393c8a58d5b43b370d144c41a6c7601256f
dogspace <rflorintsev@gmail.com>
2019-11-24 15:18:43 -0600

    try to let social bots get meta using express-device

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // server.js
 // load the things we need
 var express = require('express');
-var bodyParser = require('body-parser')
-var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var device = require('express-device');
 var path = require('path');
 var app = express();
 
@@ -109,12 +110,13 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(cookieParser())
+app.use(device.capture());
 app.use(bodyParser.urlencoded({ extended: false }))
 // use res.render to load up an ejs view file
 
 // index page 
 app.get('*', function(req, res, next) {
-  if (req.cookies.accepted == "true" || req.path == "/ohnoes") {
+  if (req.cookies.accepted == "true" || req.path == "/ohnoes" || req.device.type == "bot") {
     next();
   } else {
     res.render('disclaimer.ejs');
