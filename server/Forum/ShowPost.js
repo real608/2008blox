b56f1b0dbd24629e6a4314af195121667a3496a8
dogspace <rflorintsev@gmail.com>
2020-07-19 15:33:07 -0500

    OH DEAR! Quickly patch sql injection!!

--- server/Forum/ShowPost.js
+++ server/Forum/ShowPost.js
@@ -5,13 +5,13 @@ module.exports = async function(database, req) {
     let offset = page * 25
 
     let res = await database.query(`
-    SELECT postid, postsubject, replies, authorid,
+    SELECT postid, postsubject, replies, authorid, lastpost
     FROM posts
-    WHERE thread = true AND subforum = $1
-    ORDER BY postdate DESC
+    WHERE parentthread = $1
+    ORDER BY postdate ASC
     LIMIT 25
     OFFSET $2;
-    `, [req.query.ForumID, offset])
+    `, [req.query.PostID, offset])
 
     return {forum: forum.find(g => g.forums.find(f => f.id == req.query.ForumID)), posts: res.rows}
 }
